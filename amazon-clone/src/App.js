79a7b59b7b782b848457b03cc702211453c5baa9
Josiah Badrina <joshbadrina@gmail.com>
2022-02-26 15:02:37 -0600

    Amazon-Clone

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+import User_Profile from './User_Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <User_Profile />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyATiM9-qmcUZvIP5i4v2e1DXbj2-BGlCHI",
+    authDomain: "clone-326ac.firebaseapp.com",
+    projectId: "clone-326ac",
+    storageBucket: "clone-326ac.appspot.com",
+    messagingSenderId: "490099293986",
+    appId: "1:490099293986:web:1fda51538fd98461c9af7b",
+    measurementId: "G-T9GKR6JFBP"
+  };
