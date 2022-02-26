5a5b391e09b06b0acf01e70ad3e299bce88f5286
bob <mariclaire.de-guia@ttu.edu>
2022-02-26 17:58:56 -0600

    UserTop updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# HackWesTX
-HackWesTX Hackathon
-
-Hello  (Josh was here.)
-
-Hello! (Gil was here.)
-
-Hello! - from MC
--- amazon-clone/src/User_Profile.js
+++ amazon-clone/src/User_Profile.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./User_Profile.css";
 import Product from "./Product"
+import ProfileTop from './ProfileTop'
 
 function User_Profile() {
   return (
-    <div className="home">
-      <div className="home__container">
+       
+      <div className="home__container">       
+        <ProfileTop 
+          icon="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg"
+          username ='User1'
+          profession='Author'
+          desc = 'Hello! My name is User1 and I am an aspiring author. I have written books like Novel1 and Novel2.'
+          link='www.User1swebsite.com'
+          followers = '100'
+          following = '200'
+          subscribers = '15'
+        />    
+
+
         <img
           className="home__image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
-<div className="home__row">
+        <div className="home__row">
           <Product
             id="12321341"
             title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
@@ -63,7 +76,6 @@ function User_Profile() {
           />
         </div>
       </div>
-    </div>
   );
 }
 
