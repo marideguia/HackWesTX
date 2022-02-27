3f94bc9e09bab09dffbc2d379c1622a6364305b8
kalanggam <62631102+kalanggam@users.noreply.github.com>
2022-02-27 04:24:55 -0600

    Merging with main branch

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -8,12 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-<<<<<<< HEAD
       <Login />
-=======
-      {/* <Post /> */}
-      <User_Profile/>
->>>>>>> 96f9e5b877fd16e33fbe988b84439a0b8a84c3eb
     </div>
   );
 }
